5d8de8a5a5c5162058dde804aed4fca3cf0bc8d0
Edwin Lin <edwinlin1987@gmail.com>
2015-06-16 15:48:52 -0700

    Adding hashing

--- db/models/user.js
+++ db/models/user.js
@@ -1,4 +1,6 @@
 var db = require('../config');
+var Promise = require('bluebird');
+var bcrypt = require('bcrypt-nodejs');
 
 var User = db.Model.extend({
   tableName: 'users',
@@ -6,6 +8,26 @@ var User = db.Model.extend({
   score: function() {
     return this.hasMany(Score);
   },
+// code borrowed from Hack Reactor
+  initialize: function(){
+    this.on('creating', this.hashPassword);
+  },
+  comparePassword: function(attemptedPassword, callback) {
+    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+      callback(isMatch);
+    });
+  },
+  hashPassword: function(){
+    var cipher = Promise.promisify(bcrypt.hash);
+    // return a promise - bookshelf will wait for the promise
+    // to resolve before completing the create action
+    return cipher(this.get('password'), null, null)
+      .bind(this)
+      .then(function(hash) {
+        this.set('password', hash);
+      });
+  }
+// end code borrowed from Hack Reactor
 });
 
 module.exports = User;
